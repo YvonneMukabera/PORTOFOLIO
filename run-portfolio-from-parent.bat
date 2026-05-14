c539ab2aa537ab162a04122ff3f03d7122762b59
MukaberaYvonne <mukabera255@gmail.com>
2026-05-14 23:56:12 +0200

    deployment

--- run-portfolio-from-parent.bat
+++ run-portfolio-from-parent.bat
@@ -1,3 +1,10 @@
 @echo off
 cd /d "%~dp0"
+echo Starting portfolio with direct email sending enabled.
+echo Enter your Gmail address and Gmail app password when asked.
+echo.
+set PORTFOLIO_MAIL_ENABLED=true
+if "%PORTFOLIO_MAIL_USERNAME%"=="" set /p PORTFOLIO_MAIL_USERNAME=Enter email username:
+if "%PORTFOLIO_MAIL_PASSWORD%"=="" set /p PORTFOLIO_MAIL_PASSWORD=Enter email app password:
+echo.
 mvn -pl PORTOFOLIO spring-boot:run
